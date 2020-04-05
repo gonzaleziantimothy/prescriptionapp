b6022e2ae50250df66097cf8732e41d0403eef84
Ian Timothy Gonzalez <gonzaleziantimothy@gmail.com>
2020-04-05 21:00:23 +0800

    changed image filename

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ function App() {
     htmlToImage.toJpeg(document.getElementById('fullPrescription'), { quality: 0.95 })
       .then(function (dataUrl) {
       var link = document.createElement('a');
-      link.download = 'my-image-name.jpeg';
+      link.download = 'RX-'+ patientInputs.patientName +'.jpeg';
       link.href = dataUrl;
       link.click();
     });
