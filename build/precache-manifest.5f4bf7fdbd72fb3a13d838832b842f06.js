5262ab51bea120edfeb2ab618955621648695fbb
Ian Timothy Gonzalez <gonzaleziantimothy@gmail.com>
2020-04-12 20:41:21 +0800

    includes gh-pages

---
+++ build/precache-manifest.5f4bf7fdbd72fb3a13d838832b842f06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a51cecb17bb72a1116f67fdef9700b50",
+    "url": "/prescriptionapp/index.html"
+  },
+  {
+    "revision": "a370e59c96885baf9882",
+    "url": "/prescriptionapp/static/css/main.9c666cb7.chunk.css"
+  },
+  {
+    "revision": "2d894ae894bdf195db50",
+    "url": "/prescriptionapp/static/js/2.01e35fc9.chunk.js"
+  },
+  {
+    "revision": "a370e59c96885baf9882",
+    "url": "/prescriptionapp/static/js/main.517ea8e9.chunk.js"
+  },
+  {
+    "revision": "8d9e38bad90a202c8476",
+    "url": "/prescriptionapp/static/js/runtime-main.32589b83.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/prescriptionapp/static/media/logo.25bf045c.svg"
+  }
+]);
